58924c28656b6530cac03a5f1d03651a01a3bf99
Damen Hannah <damen@damenhannah.com>
2022-10-16 01:50:15 -0600

    Preliminary vanilla implementation.

--- examples/vanilla/src/main.ts
+++ examples/vanilla/src/main.ts
@@ -1,23 +1,16 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import { setupCounter } from './counter'
+import "./style.css";
+import { setupHexEditor } from "./hexEditor";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+<main class="flex flex-col h-screen items-center justify-center bg-[rgb(50,50,50)] text-white">
+	<div class="min-h-fit overflow-hidden">
+		<div id="hex-editor"></div>
+	</div>
+</main>
+`;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+setupHexEditor(
+    document.querySelector<HTMLDivElement>("#hex-editor")!,
+    new Uint8Array(524_288).fill(0x00).map(() => Math.floor(Math.random() * 256)),
+    32
+);
